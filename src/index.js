543de6e6b4ab1ebf87f7ffb18aef30dce6a6b48f
parsianNight <tony20space@gmail.com>
2022-08-23 19:48:20 +0200

    trying to fix things

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const UserRoutes = require('./routers/user')
 const TaskRoutes = require('./routers/task')
 
 const app = express()
-const port = process.env.PORT || 3000 
+const port = process.env.PORT
 
 
 app.use(express.json())
