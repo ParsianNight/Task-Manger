4600910666bcf8f16be415436a1371a93e3195e5
parsianNight <tony20space@gmail.com>
2022-08-23 19:57:55 +0200

    It's ready Guys, finally :D

--- src/index.js
+++ src/index.js
@@ -12,27 +12,6 @@ app.use(UserRoutes)
 app.use(TaskRoutes)
 
 
-
-
-
-
-
 app.listen(port, ()=> {
 })
-
-
-// const Task = require('./models/task')
-// const User = require('./models/user')
-// const main = async () => {
-//     // One Way to do it .. 
-//     // const task = await Task.findById('62f832b376c36b4f6e7acf54')
-//     // await task.populate('owner')
-//     // console.log(task)
-    // Other Way..
-//     const user = await User.findById('62f8304c332fca66a8e7ccb7')
-//     await user.populate('tasks')
-//     console.log(user.tasks)
-// }
-// main()
- 
 
